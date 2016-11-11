fa20916f1e775eb47ea09a25b5d47d5169b88e20
Tibor Molnár <fatalaa@hotmail.com>
2016-11-11 03:21:31 +0100

    Fix compile error caused by a deleted file

--- DueDate/Presenter.h
+++ DueDate/Presenter.h
@@ -9,7 +9,6 @@
 #import <Foundation/Foundation.h>
 
 #import "DateHelper.h"
-#import "InputValidator.h"
 #import "ViewController.h"
 
 @protocol PresenterInterface <NSObject>
@@ -23,7 +22,6 @@
 
 @property (nonatomic, weak) id<View> view;
 @property (nonatomic, strong) id<DateHelperInterface> dateHelper;
-@property (nonatomic, strong) id<InputValidating> validator;
 
 - (instancetype)initWithView:(id<View>)view dateHelper:(id<DateHelperInterface>)dateHelper;
 
